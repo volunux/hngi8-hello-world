c5f4b28f8e669155f047db57fef4a32adbadc045
volunux <37976953+volunux@users.noreply.github.com>
2021-08-17 17:29:07 -0700

    Hngi8 Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+/**
+*This method simply prints my name to the output stream. 
+**/
+
+	public static void main(String args[]) {
+
+		System.out.println("Yusuf Yusuf Musa");
+	}	
+}
